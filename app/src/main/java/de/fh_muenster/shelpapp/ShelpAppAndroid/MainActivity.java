514f4f185c48e7477082fb257f76e5766cfb9704
roman0420 <busch.roman20@gmail.com>
2015-05-12 19:00:13 +0200

    md5_login

--- app/src/main/java/de/fh_muenster/shelpapp/ShelpAppAndroid/MainActivity.java
+++ app/src/main/java/de/fh_muenster/shelpapp/ShelpAppAndroid/MainActivity.java
@@ -7,11 +7,20 @@ import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.NoSuchElementException;
 
 import de.fh_muenster.shelpapp.R;
 import de.fh_muenster.shelpapp.ShelpApp.Exceptions.InvalidLoginException;
@@ -19,6 +28,9 @@ import de.fh_muenster.shelpapp.ShelpApp.User;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private String usern, userp;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,7 @@ public class MainActivity extends ActionBarActivity {
         //Textfeld anhand der ID suchen und Passwort setzen
         EditText pw = (EditText) findViewById(R.id.editTextPassword);
         pw.setText(password);
+
     }
 
 
@@ -68,7 +81,7 @@ public class MainActivity extends ActionBarActivity {
         startActivity(i);
     }
 
-    public void login(View loginView) {
+    /*public void login(View loginView) {
 
         //E-Mail und Passwort aus den SharedPreferences laden
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(loginView.getContext());
@@ -77,11 +90,14 @@ public class MainActivity extends ActionBarActivity {
         //Passwort als String in Variable speichern
         String password = prefs.getString("password", "");
 
-        /**Prüfung ob E-Mail und Passwort mit den Daten aus dem Mockobjekt
-         * mit den SharedPreferences übereinstimmen. Wenn die Daten passen, wird
-         * der LoginTask ausgeführt. Falls die Daten nicht übereinstimmen, wird eine
-         * Fehlermeldung ausgegeben und als Popup angezeigt.
-         */
+        */
+
+    /**
+     * Prüfung ob E-Mail und Passwort mit den Daten aus dem Mockobjekt
+     * mit den SharedPreferences übereinstimmen. Wenn die Daten passen, wird
+     * der LoginTask ausgeführt. Falls die Daten nicht übereinstimmen, wird eine
+     * Fehlermeldung ausgegeben und als Popup angezeigt.
+     *//*
         if(!"".equals(username) && !"".equals(password)) {
             //LoginTask erstellen
             LoginTask loginTask = new LoginTask(loginView.getContext());
@@ -96,22 +112,67 @@ public class MainActivity extends ActionBarActivity {
             Toast toast = Toast.makeText(loginView.getContext(), text, duration);
             toast.show();
         }
+    }*/
+    public void login(View loginView) {
+        EditText un = (EditText) findViewById(R.id.editUsername);
+        EditText pw = (EditText) findViewById(R.id.editTextPassword);
+        usern = un.getText().toString();
+        userp = pw.getText().toString();
+        computeMD5Hash(userp);
+
+        if ((usern != null && usern.equals("")) || (userp != null && userp.equals("")) {
+            if (result.equals("d41d8cd98f00b204e9800998ecf8427e")) {
+                //LoginTask erstellen
+                LoginTask loginTask = new LoginTask(loginView.getContext());
+                //LoginTask ausführen
+                loginTask.execute(usern, userp);
+            } else {
+                //Toast anzeigen
+                CharSequence text = "Fehlende Logindaten bitte in den Einstellungen eintragen!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(loginView.getContext(), text, duration);
+                toast.show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Enter your credentials..", Toast.LENGTH_LONG).show();
+        }
     }
 
-    private class LoginTask extends AsyncTask<String, Integer, User>
-    {
+
+    public void computeMD5Hash(String password) {
+        try {
+            // Create MD5 Hash d41d8cd98f00b204e9800998ecf8427e
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(password.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            StringBuffer MD5Hash = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++) {
+                String h = Integer.toHexString(0xFF & messageDigest[i]);
+                while (h.length() < 2)
+                    h = "0" + h;
+                MD5Hash.append(h);
+            }
+            result = MD5Hash.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private class LoginTask extends AsyncTask<String, Integer, User> {
         private Context context;
 
         //Dem Konstruktor der Klasse wird der aktuelle Kontext der Activity übergeben
         //damit auf die UI-Elemente zugegriffen werden kann und Intents gestartet werden können, usw.
-        public LoginTask(Context context)
-        {
+        public LoginTask(Context context) {
             this.context = context;
         }
 
         @Override
-        protected User doInBackground(String... params){
-            if(params.length != 2)
+        protected User doInBackground(String... params) {
+            if (params.length != 2)
                 return null;
             String username = params[0];
             String password = params[1];
@@ -125,13 +186,11 @@ public class MainActivity extends ActionBarActivity {
             return null;
         }
 
-        protected void onProgessUpdate(Integer... values)
-        { }
+        protected void onProgessUpdate(Integer... values) {
+        }
 
-        protected void onPostExecute(User result)
-        {
-            if(result != null)
-            {
+        protected void onPostExecute(User result) {
+            if (result != null) {
                 //erfolgreich eingeloggt
                 ShelpAppApplication myApp = (ShelpAppApplication) getApplication();
                 myApp.setUser(result);
@@ -145,9 +204,7 @@ public class MainActivity extends ActionBarActivity {
                 //Nächste Activity anzeigen
                 Intent i = new Intent(context, shelp_activity.class);
                 startActivity(i);
-            }
-            else
-            {
+            } else {
                 //Toast anzeigen
                 CharSequence text = "Login fehlgeschlagen!";
                 int duration = Toast.LENGTH_SHORT;
