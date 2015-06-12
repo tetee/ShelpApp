ae1b697dd6aa111de4b45446d918fd25fd3810d4
Tomoffel <jwerner.ms@gmail.com>
2015-06-12 12:27:59 +0200

    Not Fix Friends

--- app/src/main/java/de/shelp/android/tasks/GetFriendsTask.java
+++ app/src/main/java/de/shelp/android/tasks/GetFriendsTask.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,8 +30,7 @@ public class GetFriendsTask extends AsyncTask<Object, Integer, List<Friendship>>
     private ShelpApplication myApp;
     private FriendsActivity friendsActivity;
 
-    private int nextAskedId =  R.id.requestFriend;
-    private int nextFriendId  =  R.id.actual;;
+    private int nextFriendId  =  R.id.searchFriend;
 
     //Dem Konstruktor der Klasse wird der aktuelle Kontext der Activity übergeben
     //damit auf die UI-Elemente zugegriffen werden kann und Intents gestartet werden können, usw.
@@ -59,31 +59,68 @@ public class GetFriendsTask extends AsyncTask<Object, Integer, List<Friendship>>
         if(result == null) {
             Toast.makeText(myApp.getApplicationContext(), "Du hast noch keine Freunde. Füge doch einfach welche hinzu.", Toast.LENGTH_SHORT).show();
         } else {
-            for( Friendship friendship: result) {
-                if(friendship.getInitiatorUser().getUserName().equals(myApp.getSession().getUser().getUserName())) {
-                    RelativeLayout ll = (RelativeLayout) friendsActivity.findViewById(R.id.relativeLayoutFriend);
+            RelativeLayout ll = (RelativeLayout) friendsActivity.findViewById(R.id.relativeLayoutFriend);
 
-                    RelativeLayout.LayoutParams relativeParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
+            RelativeLayout.LayoutParams relativeParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
+            relativeParams.addRule(RelativeLayout.BELOW, nextFriendId);
+            this.nextFriendId++;
+            TextView et = new TextView(context);
+            et.setId(nextFriendId);
+            et.setTextSize(20);
+            et.setText("Anfragen");
+            ll.addView(et, relativeParams);
+
+            for( Friendship friendship: result) {
+                if (friendship.getInitiatorUser().getUserName().equals(myApp.getSession().getUser().getUserName())) {
+                    RelativeLayout ll2 = (RelativeLayout) friendsActivity.findViewById(R.id.relativeLayoutFriend);
+
+                    RelativeLayout.LayoutParams relativeParams2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
                     relativeParams.addRule(RelativeLayout.BELOW, nextFriendId);
                     this.nextFriendId++;
-                    TextView et = new TextView(context);
-                    et.setId(nextFriendId);
-                    et.setTextSize(20);
-                    et.setText(friendship.toString());
-                    ll.addView(et, relativeParams);
-                } else {
-                    RelativeLayout ll = (RelativeLayout) friendsActivity.findViewById(R.id.relativeLayoutFriend);
+                    TextView et2 = new TextView(context);
+                    et2.setId(nextFriendId);
+                    et2.setTextSize(20);
+                    et2.setText(friendship.toString());
+                    ll2.addView(et2, relativeParams2);
 
-                    RelativeLayout.LayoutParams relativeParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
-                    relativeParams.addRule(RelativeLayout.BELOW, nextAskedId);
-                    this.nextAskedId++;
-                    TextView et = new TextView(context);
-                    et.setId(nextAskedId);
-                    et.setTextSize(20);
-                    et.setText(friendship.toString());
-                    ll.addView(et, relativeParams);
+                    RelativeLayout ll4 = (RelativeLayout) friendsActivity.findViewById(R.id.relativeLayoutFriend);
+
+                    RelativeLayout.LayoutParams relativeParams4 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
+                    relativeParams.addRule(RelativeLayout.BELOW, nextFriendId);
+                    this.nextFriendId++;
+                    Button et4 = new Button(context);
+                    et4.setId(nextFriendId);
+                    et4.setTextSize(20);
+                    et4.setText("Löschen");
+                    ll4.addView(et4, relativeParams4);
                 }
             }
+
+//            RelativeLayout ll4 = (RelativeLayout) friendsActivity.findViewById(R.id.relativeLayoutFriend);
+//
+//            RelativeLayout.LayoutParams relativeParams4 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
+//            relativeParams.addRule(RelativeLayout.BELOW, nextFriendId);
+//            this.nextFriendId++;
+//            TextView et4 = new TextView(context);
+//            et4.setId(nextFriendId);
+//            et4.setTextSize(20);
+//            et4.setText("Freunde " + "");
+//            ll4.addView(et4, relativeParams4);
+//
+//            for( Friendship friendship: result) {
+//                if (friendship.getRecipientUser().getUserName().equals(myApp.getSession().getUser().getUserName())){
+//                    RelativeLayout ll3 = (RelativeLayout) friendsActivity.findViewById(R.id.relativeLayoutFriend);
+//
+//                    RelativeLayout.LayoutParams relativeParams3 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
+//                    relativeParams.addRule(RelativeLayout.BELOW, nextFriendId);
+//                    this.nextFriendId++;
+//                    TextView et3 = new TextView(context);
+//                    et3.setId(nextFriendId);
+//                    et3.setTextSize(20);
+//                    et3.setText(friendship.toString() + "");
+//                    ll3.addView(et3, relativeParams3);
+//                }
+//            }
         }
     }
 }
